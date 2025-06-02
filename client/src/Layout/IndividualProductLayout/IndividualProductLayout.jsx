36a63c2e4a1e228a3cc28c2bdc61f40cb3d3be34
SAYUJYA SATYAL <sayujya57@gmail.com>
2025-06-02 07:17:25 +0545

    Add Instruction for Individual Product

--- client/src/Layout/IndividualProductLayout/IndividualProductLayout.jsx
+++ client/src/Layout/IndividualProductLayout/IndividualProductLayout.jsx
@@ -4,7 +4,8 @@ const IndividualProduct = () => {
   const { productId } = useParams();
   // Steps to do
   // 1. Backend bata when productId aaucha then return productId, in routes the path will be "/product/:productId"
-  // 2. Receive that api and use useEffect to get the result inside this function
+  // 2. Receive that api and through oublic state inside the GlobalState get the result in an object
+  // 3. UseEffect to call that function and the product
   // 3. Style that product
   return (
     <div className="min-h-screen flex justify-center items-center">
