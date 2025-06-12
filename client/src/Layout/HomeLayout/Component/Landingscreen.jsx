68ff24c4ff1b829c3bbe3950695efa54c17b17a0
Sneha Acharya <sneha11acharya@gmail.com>
2025-06-12 06:42:17 +0545

    link

--- client/src/Layout/HomeLayout/Component/Landingscreen.jsx
+++ client/src/Layout/HomeLayout/Component/Landingscreen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link
 import "../Styles/Landingscreen.css";
 import mainImage from "../Images/main.png";
 
@@ -7,14 +8,14 @@ const Landingscreen = () => {
     <section className="main-home flex justify-around items-center">
       <div className="main-text p-20">
         <h3 className="font-second text-3xl">
-          Support Sutainable Fashion<i className="bx bx-recycle"></i>
+          Support Sustainable Fashion<i className="bx bx-recycle"></i>
         </h3>
         <h2>most items under Rs.999/-</h2>
         <br />
         <p>Why pay full price when you can pay half and slay twice as hard?</p>
-        <a href="#" className="main-btn">
+        <Link to="/products" className="main-btn">
           Thrift Now<i className="bx bx-right-arrow-alt"></i>
-        </a>
+        </Link>
       </div>
       <div className="justify-self-end hidden md:inline-block">
         <img src={mainImage} alt="Sustainable Fashion" />
